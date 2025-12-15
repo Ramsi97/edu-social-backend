54f78a88f252b5128d1668cc95d1dd0ad32f0439
Ramsi97 <ramadan2jemal@gmail.com>
2025-12-15 23:58:15 +0300

    auth implementing

---
+++ internal/auth/repository/interfaces/user_repository.go
@@ -0,0 +1,14 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/Ramsi97/edu-social-backend/internal/auth/domain"
+)
+
+
+type UserRepository interface {
+	Create (ctx context.Context, user *domain.User) error
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
+	FindByStudentId(ctx context.Context, studentId string)(*domain.User, error)
+}
